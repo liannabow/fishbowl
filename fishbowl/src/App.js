6757ded747ba8d85214b206390f23207dc9d8cd0
Lianna <liannabow@gmail.com>
2020-04-29 16:06:04 -0400

    added start button

--- fishbowl/src/App.js
+++ fishbowl/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <Button variant="outline-light">Start</Button>
       </header>
     </div>
   );
