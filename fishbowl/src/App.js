6858f6acb7b1b490fc3fa43d06a1505545336e8c
Lianna <liannabow@gmail.com>
2020-05-01 11:37:57 -0400

    router exists but no links used yet

--- fishbowl/src/App.js
+++ fishbowl/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import SplashScreen from './components/SplashScreen';
+import TeamSignup from './components/TeamSignup';
 
 function App() {
   return (
-    <SplashScreen/>
+    <Switch>
+      <Route path="/" component={SplashScreen}/>
+      <Route path="/TeamSignup" component={TeamSignup}/>
+    </Switch>
   );
 }
 
---
+++ fishbowl/src/components/TeamSignup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TeamSignup() {
+  return (
+    <h1>Team Signup</h1>
+  );
+}
+
+export default TeamSignup;
