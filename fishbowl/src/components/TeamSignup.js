210cf6b3758f0bc536410b075f976ed16cc0f658
Lianna <liannabow@gmail.com>
2020-05-01 13:42:07 -0400

    all items on page, need vertical spacing

--- fishbowl/src/components/TeamSignup.js
+++ fishbowl/src/components/TeamSignup.js
@@ -1,8 +1,73 @@
 import React from 'react';
+import './TeamSignup.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 
 function TeamSignup() {
   return (
-    <h1>Team Signup</h1>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Team Signup</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8}>
+          <Form.Control placeholder="Player Name" />
+        </Col>
+        <Col>
+          <Button variant="primary">Add</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div align="center">
+            <Button variant="secondary">Team 1</Button>
+            <Table striped bordered hover>
+              <tbody>
+                <tr>
+                  <td>Name 1</td>
+                </tr>
+                <tr>
+                  <td>Name 2</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>          
+        </Col>
+        <Col>
+          <div align="center">
+            <Button variant="secondary">Team 2</Button>
+          </div>
+          <Table striped bordered hover>
+            <tbody>
+              <tr>
+                <td>Name 3</td>
+              </tr>
+              <tr>
+                <td>Name 4</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div align="center">
+            <Button variant="warning">Shuffle Teams</Button>
+          </div>
+        </Col>
+        <Col>
+          <div align="center">
+            <Button variant="success">Next</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
