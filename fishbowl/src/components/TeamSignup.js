2c54fe6db46d42440f0f4eb18df8331613077b72
Lianna <liannabow@gmail.com>
2020-05-04 11:43:42 -0400

    changed from function to component

--- fishbowl/src/components/TeamSignup.js
+++ fishbowl/src/components/TeamSignup.js
@@ -7,68 +7,73 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 
-function TeamSignup() {
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>Team Signup</h1>
-        </Col>
-      </Row>
-      <Row className="Row">
-        <Col xs={8}>
-          <Form.Control type="text" placeholder="Player Name" />
-        </Col>
-        <Col>
-          <Button variant="primary">Add</Button>
-        </Col>
-      </Row>
-      <Row className="Row">
-        <Col>
-          <div align="center">
-            <Button variant="secondary">Team 1</Button>
+class TeamSignup extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h1>Team Signup</h1>
+          </Col>
+        </Row>
+        <Row className="Row">
+          <Col xs={8}>
+            <Form.Control
+              type="text"
+              placeholder="Player Name"
+              />
+          </Col>
+          <Col>
+            <Button variant="primary">Add</Button>
+          </Col>
+        </Row>
+        <Row className="Row">
+          <Col>
+            <div align="center">
+              <Button variant="secondary">Team 1</Button>
+              <Table striped bordered hover>
+                <tbody>
+                  <tr>
+                    <td>Name 1</td>
+                  </tr>
+                  <tr>
+                    <td>Name 2</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>          
+          </Col>
+          <Col>
+            <div align="center">
+              <Button variant="secondary">Team 2</Button>
+            </div>
             <Table striped bordered hover>
               <tbody>
                 <tr>
-                  <td>Name 1</td>
+                  <td>Name 3</td>
                 </tr>
                 <tr>
-                  <td>Name 2</td>
+                  <td>Name 4</td>
                 </tr>
               </tbody>
             </Table>
-          </div>          
-        </Col>
-        <Col>
-          <div align="center">
-            <Button variant="secondary">Team 2</Button>
-          </div>
-          <Table striped bordered hover>
-            <tbody>
-              <tr>
-                <td>Name 3</td>
-              </tr>
-              <tr>
-                <td>Name 4</td>
-              </tr>
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <div align="center">
-            <Button variant="warning">Shuffle Teams</Button>
-          </div>
-        </Col>
-        <Col>
-          <div align="center">
-            <Button variant="success">Next</Button>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div align="center">
+              <Button variant="warning">Shuffle Teams</Button>
+            </div>
+          </Col>
+          <Col>
+            <div align="center">
+              <Button variant="success">Next</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }  
 }
 
 export default TeamSignup;
